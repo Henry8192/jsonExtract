8c69f8351a9ed597997bfe18ecae6cde89b4cccd
Henry8192 <Henry8192@hotmail.com>
2022-07-04 10:38:23 -0700

    bugged multiple file extract

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import json
-with open('xiaoxianruo_jsons/xiaoxianruo_0_00000_keypoints.json') as input:
-    data = json.load(input)
-    count = 0
-    for item in data["people"][0]["pose_keypoints_2d"]:
-        count += 1
-        print(item, ' ',end = ""),
-        if (count % 3 == 0):
-            print()
+import os
+path = "xiaoxianruo_jsons"
+files = os.listdir(path)
+for file in files:
+    with open(f"{path}/{file}") as input:
+        data = json.load(input)
+        count = 0
+        for item in data["people"][0]["pose_keypoints_2d"]:
+            count += 1
+            print(item, ' ', end=""),
+            if (count % 3 == 0):
+                print()
+
