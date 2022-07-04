3ffddcfb3230b0759957ffe7643bdf227bf2dae7
Henry8192 <Henry8192@hotmail.com>
2022-07-04 11:29:03 -0700

    Put results in folder

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import json
 import os
+# You can change the path name here (which is your folder name)
 path = "xiaoxianruo_jsons"
 files = os.listdir(path)
+files.sort()
+
+fileNum = 0
+
 for file in files:
     with open(f"{path}/{file}") as input:
+        os.makedirs(os.path.dirname(f"result/{file}"), exist_ok=True)
+        output = open(f"result/{fileNum}.txt", 'w')
         data = json.load(input)
         count = 0
         for item in data["people"][0]["pose_keypoints_2d"]:
             count += 1
-            print(item, ' ', end=""),
+            output.write(str(item)+' '),
             if (count % 3 == 0):
-                print()
+                output.write("\n")
+    fileNum += 1
 
+output.close()
