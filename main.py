58357e90e211043d1173d5db86fcd2ac1a60b8aa
Henry8192 <Henry8192@hotmail.com>
2022-07-05 11:52:48 -0700

    formatted output file name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import os
-# You can change the path name here (which is your folder name)
+# You can change input files path below
 path = "xiaoxianruo_jsons"
 files = os.listdir(path)
 files.sort()
@@ -10,7 +10,10 @@ fileNum = 0
 for file in files:
     with open(f"{path}/{file}") as input:
         os.makedirs(os.path.dirname(f"result/{file}"), exist_ok=True)
-        output = open(f"result/{fileNum}.txt", 'w')
+        
+        # %05d means the number is a 5-digit integer.
+        # You can change this number to %06d to get a 6-digit-numbered file name.
+        output = open(f"result/%05d.txt" % fileNum, 'w')
         data = json.load(input)
         count = 0
         for item in data["people"][0]["pose_keypoints_2d"]:
